797e50e8c557355d8fd3d3fe950740db90fe36ac
Basivireddy <d.basivireddy@gmail.com>
2020-08-15 11:21:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http - require(http);
 const hostname = "0.0.0.0";
 const port = "80";
 
-const server = http.createServer{(req, res) => {
+const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
       res.end('Hello World');
